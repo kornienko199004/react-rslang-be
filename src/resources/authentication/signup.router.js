087be6d3fe30478ee1072c0b875e127e5715cba3
Ilya Yeliseyeu <Ilya.Yeliseyeu@ibm.com>
2021-03-23 22:45:18 +0300

    feat: add auth

---
+++ src/resources/authentication/signup.router.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const { OK } = require('http-status-codes');
+
+const userService = require('../users/user.service');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __dirname);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${file.fieldname}-${Date.now()}`);
+  }
+});
+
+// init upload as middleware
+const upload = multer({ storage });
+
+router.route('/').post(upload.single('image'), async (req, res) => {
+  const { email, password } = req.body;
+  const user = {
+    email,
+    password,
+    photo: {
+      data: req.file // eslint-disable-next-line no-sync
+        ? fs.readFileSync(path.join(__dirname, req.file.filename))
+        : null,
+      contentType: 'image/png'
+    }
+  };
+
+  await userService.save(user);
+  const auth = await userService.authenticate(req.body);
+  res.status(OK).json({
+    message: 'Authenticated',
+    ...auth
+  });
+});
+
+module.exports = router;
